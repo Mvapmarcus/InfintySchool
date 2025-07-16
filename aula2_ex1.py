4308c00c8022189138c7e07e5d7b074bb1ecf592
Marcus Vinicius de Almeida Perdigão <mvapmarcus@gmail.com>
2025-07-16 20:02:13 -0300

    Add files via upload

---
+++ aula2_ex1.py
@@ -0,0 +1,17 @@
+n1 = float(input('Digite um número'))
+n2 = float(input('Digite um número'))
+soma = n1 + n2
+multiplicação = n1 * n2
+subtração = n1 - n2
+divisão = n1 / n2
+exponenciação = n1 ** n2
+resto = n1 % n2
+divisão_int = n1 // n2
+
+print(f'A soma é {soma}')
+print(f'A subtração é {subtração}')
+print(f'A multiplicação é {multiplicação}')
+print(f'A divisão é {divisão}')
+print(f'a exponenciação é {exponenciação}')
+print(f'o resto é {resto}')
+print(f'A divisão inteira é {divisão_int}')
