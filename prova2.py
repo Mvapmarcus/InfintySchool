5001477fe05cb9ec9fa6e333586bb9e39a9fa87a
Marcus <mvapmarcus@gmail.com>
2025-07-17 12:56:12 -0300

    Update prova2.py

--- prova2.py
+++ prova2.py
@@ -1,9 +1,9 @@
 numero = float(input("Digite um número: "))
 
 if numero > 0:
-    print(f'O número {numero} é positivo.')   
+    print(f'O número é positivo.')   
 elif numero < 0:
-    print(f'O número {numero} é negativo.')   
+    print(f'O número é negativo.')   
 else:
-    print(f'O número {numero} é zero.')
+    print(f'O número é zero.')
     
